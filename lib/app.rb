154a0275c9226fefb4d995ddbc3e07ad4c944bbd
Daniel Watts <d.watts15@outlook.com>
2022-02-01 17:52:38 +0000

    Completed Sinatra:erb

--- lib/app.rb
+++ lib/app.rb
@@ -5,20 +5,6 @@ get '/' do
   "hello!"
 end
 
-get '/secret' do
-  "top secret"
-end
-
-get '/newroute' do
-  "a new route"
-end
-
-get '/test' do
-  "did not restart sinatra"
-end
-
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-     <img src='http://placekitten.com/500/500'>
-   </div>"
+  erb(:index)
 end
